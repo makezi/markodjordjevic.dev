d1d2faac772ca2a676d1ed1247751d5aad4d7bbd
Marko Djordjevic <makezi08@gmail.com>
2021-01-17 19:27:44 +1100

    Made header responsive

--- components/header/header.js
+++ components/header/header.js
@@ -1,14 +1,23 @@
+import * as React from 'react';
 import {
   Box,
   Container,
   Heading,
   Link,
-  Flex,
   Spacer,
-  Text
+  Text,
+  Button,
+  Slide,
+  Portal,
+  Stack,
+  useDisclosure,
+  useBreakpoint
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
+import { HiMenuAlt3, HiX } from 'react-icons/hi';
+
+const HEADER_HEIGHT = '100px';
 
 function NavLink({ href, emote, children }) {
   const { asPath } = useRouter();
@@ -21,6 +30,7 @@ function NavLink({ href, emote, children }) {
         py={2}
         px={4}
         fontFamily="heading"
+        fontSize={['2xl', '2xl', 'lg']}
         fontWeight="medium"
         letterSpacing="-1px"
         display="flex"
@@ -47,15 +57,63 @@ function NavLink({ href, emote, children }) {
 }
 
 function Navigation() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const breakpoint = useBreakpoint();
+
+  React.useEffect(() => {
+    if (breakpoint !== 'base' && breakpoint !== 'sm') {
+      onClose();
+    }
+  }, [breakpoint, onClose]);
+
+  const onToggle = () => (isOpen ? onClose() : onOpen());
+
   return (
-    <Flex>
-      <NavLink href="/blog" emote="📝">
-        blog
-      </NavLink>
-      <NavLink href="/" emote="💁🏻‍♂️">
-        about
-      </NavLink>
-    </Flex>
+    <>
+      <Box display={['block', 'block', 'none']}>
+        <Button variant="ghost" onClick={onToggle}>
+          {isOpen ? <HiX size={24} /> : <HiMenuAlt3 size={24} />}
+        </Button>
+        <MobileMenu isOpen={isOpen} />
+      </Box>
+      <Box display={['none', 'none', 'block']}>
+        <Stack direction="row">
+          <NavLink href="/blog" emote="📝">
+            blog
+          </NavLink>
+          <NavLink href="/" emote="💁🏻‍♂️">
+            about
+          </NavLink>
+        </Stack>
+      </Box>
+    </>
+  );
+}
+
+function MobileMenu({ isOpen }) {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <Portal>
+      <Slide
+        direction="right"
+        in
+        style={{ zIndex: 'modal', top: HEADER_HEIGHT }}
+      >
+        <Box bg="white" height="100%" py={6}>
+          <Stack align="center" spacing={4}>
+            <NavLink href="/blog" emote="📝">
+              blog
+            </NavLink>
+            <NavLink href="/" emote="💁🏻‍♂️">
+              about
+            </NavLink>
+          </Stack>
+        </Box>
+      </Slide>
+    </Portal>
   );
 }
 
@@ -69,14 +127,19 @@ function Header() {
       sx={{ backdropFilter: 'blur(20px)' }}
       bg="whiteAlpha.700"
     >
-      <Container maxW="4xl" p={[4, 4, 8]}>
-        <Flex alignItems="center">
-          <Heading size="lg" as="h1">
-            marko djordjevic
-          </Heading>
-          <Spacer />
-          <Navigation />
-        </Flex>
+      <Container
+        maxW="4xl"
+        px={[4, 4, 8]}
+        height={HEADER_HEIGHT}
+        display="flex"
+        width="100%"
+        alignItems="center"
+      >
+        <Heading size="lg" as="h1">
+          marko djordjevic
+        </Heading>
+        <Spacer />
+        <Navigation />
       </Container>
     </Box>
   );
