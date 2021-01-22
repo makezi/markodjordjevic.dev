f5b33b1213d8736c043c32447cb812a89227860d
Marko Djordjevic <makezi08@gmail.com>
2021-01-22 19:54:23 +1100

    Add title package, capitalized blog title

--- layouts/index.js
+++ layouts/index.js
@@ -1,4 +1,5 @@
 import { Stack, Text, Heading } from '@chakra-ui/react';
+import capitalizeTitle from 'title';
 
 import { Layout } from '@/components/layout';
 
@@ -6,10 +7,12 @@ export default function BlogLayout({ children, frontMatter }) {
   console.log('frontMatter', frontMatter);
   const { title, date } = frontMatter;
 
+  const capitalizedTitle = capitalizeTitle(title);
+
   return (
     <Layout>
       <Stack spacing={6}>
-        <Heading size="3xl">{title}</Heading>
+        <Heading size="3xl">{capitalizedTitle}</Heading>
         {children}
       </Stack>
     </Layout>
